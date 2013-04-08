05c11c841c707a22577058c2029d094d3f43f276
Gordon Tyler <gordon@doxxx.net>
2013-04-07 23:40:37 -0400

    Move Item building code into WowDbApi.

--- src/main/scala/net/doxxx/spatula/Main.scala
+++ src/main/scala/net/doxxx/spatula/Main.scala
@@ -1,13 +1,10 @@
 package net.doxxx.spatula
 
 import akka.actor._
-import akka.contrib.throttle.Throttler._
-import akka.contrib.throttle.TimerBasedThrottler
 import akka.pattern._
 import akka.util.Timeout
 import akka.event.Logging
 import com.typesafe.config.ConfigFactory
-import spray.json.{JsObject, DefaultJsonProtocol}
 import java.io.{BufferedWriter, FileWriter, File}
 import scala.util.{Failure, Success}
 import scala.concurrent._
@@ -16,7 +13,6 @@ import ExecutionContext.Implicits.global
 import scala.io.Source
 
 object Main {
-  import DefaultJsonProtocol._
   import WowDbApi._
 
   val config = ConfigFactory.load()
@@ -39,19 +35,14 @@ object Main {
 
     val itemIds = Source.fromFile(inFile).getLines().map(_.split(',')).flatten.map(_.toInt).toSeq
 
-
     val fs = for (id <- itemIds) yield {
-      val f = (api ? FetchItem(id)).mapTo[JsObject]
-      val f2 = for (
-        obj <- f;
-        item <- buildItem(obj)
-      ) yield item
+      val f = (api ? FetchItem(id)).mapTo[Item]
 
-      f2.onSuccess {
+      f.onSuccess {
         case i: Item => log.info("Fetched item: {} -> {}", i.id, i.name)
       }
 
-      f2
+      f
     }
 
     val f = Future.sequence(fs)
@@ -126,57 +117,5 @@ object Main {
 
   case class CategorizedItem(category: String, id: Int, value: Int)
 
-  val restoresRE = "Restores ([0-9,]+) (health|mana)( and ([0-9,]+) mana)?".r
-  val comboRE = "Restores ([0-9,]+) health and ([0-9,]+) mana".r
-  val healthRE = "Restores ([0-9,]+) health".r
-  val manaRE = "Restores ([0-9,]+) mana".r
-  val commaRE = ",".r
-
-  val buffRE = "If you spend at least ([0-9]+) seconds eating you will become well fed and gain ([0-9]+) ([^ ]+) for ([0-9]+) (.+).".r
-
-  def parseNumber(s: String): Int = {
-    commaRE.replaceAllIn(s, "").toInt
-  }
-
-  def buildSpellEffects(spellId: Int): Future[Seq[SpellEffect]] = {
-    (api ? FetchSpell(spellId)).mapTo[JsObject].map { spellObj =>
-      val desc = spellObj.fields("AuraDescriptionParsed").convertTo[String]
-//      println(desc)
-      val restore: Seq[SpellEffect] = restoresRE.findFirstIn(desc) match {
-        case Some(comboRE(health, mana)) => Seq(HealthAndMana(parseNumber(health), parseNumber(mana)))
-        case Some(healthRE(health)) => Seq(Health(parseNumber(health)))
-        case Some(manaRE(mana)) => Seq(Mana(parseNumber(mana)))
-        case None => Seq.empty
-      }
-      val buff: Seq[SpellEffect] = buffRE.findFirstIn(desc) match {
-        case Some(buffDesc) => Seq(Buff(buffDesc))
-        case None => Seq.empty
-      }
-      restore ++ buff
-    }
-  }
-
-  def buildItem(itemObj: JsObject): Future[Item] = {
-    val id = itemObj.fields("ID").convertTo[Int]
-    val name = itemObj.fields("Name").convertTo[String]
-    val flags1 = itemObj.fields("Flags1").convertTo[Int]
-    val conjured = (flags1 & 0x2) == 0x2
-    val spells = itemObj.fields("Spells").convertTo[Seq[Map[String,Int]]]
-    val spellEffects = spells.map { spellObj =>
-      val spellId = spellObj("SpellID")
-      buildSpellEffects(spellId)
-    }
-    Future.sequence(spellEffects).map { effects =>
-      Item(id, name, conjured, effects.flatten)
-    }
-  }
-
-  sealed trait SpellEffect
-  sealed trait Refreshment extends SpellEffect
-  case class Health(amount: Int) extends Refreshment
-  case class Mana(amount: Int) extends Refreshment
-  case class HealthAndMana(health: Int, mana: Int) extends Refreshment
-  case class Buff(desc: String) extends SpellEffect
-  case class Item(id: Int, name: String, conjured: Boolean, effects: Seq[SpellEffect])
 
 }
--- src/main/scala/net/doxxx/spatula/WowDbApi.scala
+++ src/main/scala/net/doxxx/spatula/WowDbApi.scala
@@ -33,36 +33,87 @@ class WowDbApi(settings: Settings) extends Actor with ActorLogging {
 
   private val pipeline: HttpRequest => Future[HttpResponse] = sendReceive(throttler)
 
-  private def toJson(r: HttpResponse): JsValue = {
+  private def toJson(r: HttpResponse): JsObject = {
     val text = r.entity.asString
     // wowdb.com returns JSON surrounded with parentheses, which must be stripped
     val trimmed = text.substring(1, text.length - 1)
-    JsonParser(trimmed)
+    JsonParser(trimmed).asJsObject
   }
 
   private def fetchItem(id: Int): Future[JsObject] = {
     itemCache.fromFuture(id) {
       log.debug("Fetching item {}", id)
-      pipeline(Get("/api/item/%d?cookieTest=1".format(id))).map(toJson).map(_.asJsObject)
+      pipeline(Get("/api/item/%d?cookieTest=1".format(id))).map(toJson)
     }
   }
 
   private def fetchSpell(id: Int): Future[JsObject] = {
     spellCache.fromFuture(id) {
       log.debug("Fetching spell {}", id)
-      pipeline(Get("/api/spell/%d?cookieTest=1".format(id))).map(toJson).map(_.asJsObject)
+      pipeline(Get("/api/spell/%d?cookieTest=1".format(id))).map(toJson)
     }
   }
 
+  import DefaultJsonProtocol._
   import WowDbApi._
 
+  val restoresRE = "Restores ([0-9,]+) (health|mana)( and ([0-9,]+) mana)?".r
+  val comboRE = "Restores ([0-9,]+) health and ([0-9,]+) mana".r
+  val healthRE = "Restores ([0-9,]+) health".r
+  val manaRE = "Restores ([0-9,]+) mana".r
+  val commaRE = ",".r
+
+  val buffRE = "If you spend at least ([0-9]+) seconds eating you will become well fed and gain ([0-9]+) ([^ ]+) for ([0-9]+) (.+).".r
+
+  def parseNumber(s: String): Int = {
+    commaRE.replaceAllIn(s, "").toInt
+  }
+
+  def buildSpellEffects(spellId: Int): Future[Seq[SpellEffect]] = {
+    fetchSpell(spellId).map { spellObj =>
+      val desc = spellObj.fields("AuraDescriptionParsed").convertTo[String]
+      val restore: Seq[SpellEffect] = restoresRE.findFirstIn(desc) match {
+        case Some(comboRE(health, mana)) => Seq(HealthAndMana(parseNumber(health), parseNumber(mana)))
+        case Some(healthRE(health)) => Seq(Health(parseNumber(health)))
+        case Some(manaRE(mana)) => Seq(Mana(parseNumber(mana)))
+        case None => Seq.empty
+      }
+      val buff: Seq[SpellEffect] = buffRE.findFirstIn(desc) match {
+        case Some(buffDesc) => Seq(Buff(buffDesc))
+        case None => Seq.empty
+      }
+      restore ++ buff
+    }
+  }
+
+  def buildItem(itemObj: JsObject): Future[Item] = {
+    val id = itemObj.fields("ID").convertTo[Int]
+    val name = itemObj.fields("Name").convertTo[String]
+    val flags1 = itemObj.fields("Flags1").convertTo[Int]
+    val conjured = (flags1 & 0x2) == 0x2
+    val spells = itemObj.fields("Spells").convertTo[Seq[Map[String,Int]]]
+    val spellEffects = spells.map { spellObj =>
+      val spellId = spellObj("SpellID")
+      buildSpellEffects(spellId)
+    }
+    Future.sequence(spellEffects).map { effects =>
+      Item(id, name, conjured, effects.flatten)
+    }
+  }
+
   def receive = {
-    case FetchItem(id) => fetchItem(id) pipeTo sender
-    case FetchSpell(id) => fetchSpell(id) pipeTo sender
+    case FetchItem(id) => fetchItem(id).flatMap(buildItem) pipeTo sender
   }
 }
 
 object WowDbApi {
   case class FetchItem(id: Int)
-  case class FetchSpell(id: Int)
+
+  sealed trait SpellEffect
+  sealed trait Refreshment extends SpellEffect
+  case class Health(amount: Int) extends Refreshment
+  case class Mana(amount: Int) extends Refreshment
+  case class HealthAndMana(health: Int, mana: Int) extends Refreshment
+  case class Buff(desc: String) extends SpellEffect
+  case class Item(id: Int, name: String, conjured: Boolean, effects: Seq[SpellEffect])
 }
